7dab380c60a77c01d3efefbd567f5808fcf9fb47
Gavin Steck <gavinmsteck@gmail.com>
2025-04-15 20:35:06 -0600

    Initial portfolio commit

---
+++ src/script.js
@@ -0,0 +1,164 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import { TextGeometry, FontLoader } from 'three/examples/jsm/Addons.js';
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI();
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+//AxesHelper
+// const axesHelper = new THREE.AxesHelper();
+// scene.add(axesHelper);
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png');
+matcapTexture.colorSpace = THREE.SRGBColorSpace;
+
+/**
+ * Fonts
+ */
+
+const fontLoader = new FontLoader();
+
+fontLoader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
+  const bevelSize = 0.02;
+  const bevelThickness = 0.03;
+  const textGeometry = new TextGeometry("Gavin Steck's Portfolio", {
+    font,
+    size: 0.5,
+    depth: 0.2,
+    curveSegments: 5,
+    bevelEnabled: true,
+    bevelThickness,
+    bevelSize,
+    bevelOffset: 0,
+    bevelSegments: 4,
+  });
+  //   textGeometry.computeBoundingBox();
+  //   textGeometry.translate(
+  //     -(textGeometry.boundingBox.max.x - bevelSize) * 0.5,
+  //     -(textGeometry.boundingBox.max.y - bevelSize) * 0.5,
+  //     -(textGeometry.boundingBox.max.z - bevelThickness) * 0.5
+  //   );
+  textGeometry.center();
+  const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+  const text = new THREE.Mesh(textGeometry, material);
+  textGeometry.addEventListener('click', () => {
+    window.location.href = 'https://github.com/gmsteck';
+  });
+  scene.add(text);
+  console.time('torusGen');
+
+  const torusGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45);
+
+  for (let i = 0; i < 100; i++) {
+    const torus = new THREE.Mesh(torusGeometry, material);
+    torus.position.x = (Math.random() - 0.5) * 10;
+    torus.position.y = (Math.random() - 0.5) * 10;
+    torus.position.z = (Math.random() - 0.5) * 10;
+
+    torus.rotation.x = Math.random() * Math.PI;
+    torus.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+    torus.scale.set(scale, scale, scale);
+    scene.add(torus);
+  }
+  console.timeEnd('torusGen');
+});
+
+/**
+ * Object
+ */
+// const cube = new THREE.Mesh(
+//   new THREE.BoxGeometry(1, 1, 1),
+//   new THREE.MeshBasicMaterial()
+// );
+
+// scene.add(cube);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  //rotate cube
+  //   cube.rotation.x = 0.1 * elapsedTime;
+  //   cube.rotation.y = -0.15 * elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
